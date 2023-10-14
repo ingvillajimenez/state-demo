bd44976e8b4e68349f2c53a8eb1e5bc05c3ce0c3
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-14 17:08:04 -0600

    WTF is super() vs super(props)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import Game from "./Game";
 import Game2 from "./Game2";
+import Demo from "./Demo";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Game2 />
+        <Game color="purple" />
+        <Demo animal="Bobcat" food="Pineapple" />
       </div>
     );
   }
 }
+
+// new Demo({ animal: "Bobcat" });
 
 export default App;
