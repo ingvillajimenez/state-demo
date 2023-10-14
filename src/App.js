e51d51bc7b6fd8285ceb5e68fa866f450a6a9b46
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-14 16:46:36 -0600

    important! alternate syntax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Game from "./Game";
+import Game2 from "./Game2";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Game />
+        <Game2 />
       </div>
     );
   }
